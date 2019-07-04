bc244529b61d42ca455c52bd97563ae78eb910fd
gauravr7 <gaurav.rathor7@gmail.com>
2019-07-04 12:30:51 +0530

    008 finished

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
 //DB Config
@@ -8,11 +12,16 @@ const db = require('./config/keys').mongoURI;
 
 //connect to mongo DB
 mongoose
-.connect(db, { useNewUrlParser: true })
-.then(() => console.log('MongoDB Connected'))
-.catch( err => console.log(err));
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello again')); 
+app.get('/', (req, res) => res.send('Hello again'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
