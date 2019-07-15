31f89d73474f228f5f131c0e25105274138c0b71
gauravr7 <gaurav.rathor7@gmail.com>
2019-07-15 12:00:25 +0530

    043 finished with login form

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
-// Register User
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
 
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+
+
+// Register User
 export const registerUser = (userData, history) => dispatch => {
    axios.post('api/users/register', userData)
       .then(result => history.push('/login'))
@@ -12,3 +16,37 @@ export const registerUser = (userData, history) => dispatch => {
          })
       );
 };
+
+// Login get user toekn
+export const loginUser = userData => dispatch => {
+   axios.post('/api/users/login', userData)
+      .then(res => {
+         //save to local storage
+         const { token } = res.data;
+
+         //Set token to local storage
+         localStorage.setItem('jwtToken', token);
+
+         //Set token to auth header
+         setAuthToken(token);
+
+         // decode token to get user data
+         const decoded = jwt_decode(token);
+
+         //set current user;
+         dispatch(setCurrentUser(decoded));
+
+      }).catch(err => dispatch({
+         type: GET_ERRORS,
+         payload: err.response.data
+      }))
+};
+
+//Set Logged in user
+
+export const setCurrentUser = (decoded) => {
+   return {
+      type: SET_CURRENT_USER,
+      payload: decoded
+   }
+} 
