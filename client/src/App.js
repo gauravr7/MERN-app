97edca6e7dc6c63b4a2ec26351fae2697f05e7f6
gauravr7 <gaurav.rathor7@gmail.com>
2019-07-25 11:22:16 +0530

    069 finished

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,7 @@ import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import NotFound from './components/not-found/NotFound';
 
 import './App.css';
@@ -72,6 +73,9 @@ function App() {
 						</Switch>
 						<Switch>
 							<PrivateRoute exact path="/add-education" component={AddEducation} />
+						</Switch>
+						<Switch>
+							<PrivateRoute exact path="/feed" component={Posts} />
 						</Switch>
 						<Route exact path="/not-found" component={NotFound} />
 					</div>
